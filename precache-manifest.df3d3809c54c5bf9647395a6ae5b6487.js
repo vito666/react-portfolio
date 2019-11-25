37ff75af2e34467580398564a2724838763a46d1
Artem Konstantinov <don666vito@yandex.ua>
2019-11-25 21:44:57 +0200

    Updates

---
+++ precache-manifest.df3d3809c54c5bf9647395a6ae5b6487.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00ca8d852bafcb39b1794549cca2315",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "fe63ddf58a9198968eee",
+    "url": "/react-portfolio/static/js/2.8a41c105.chunk.js"
+  },
+  {
+    "revision": "269556d92405ef8e1918",
+    "url": "/react-portfolio/static/js/main.3153335a.chunk.js"
+  },
+  {
+    "revision": "c0bf13b4e58ea1f53b22",
+    "url": "/react-portfolio/static/js/runtime-main.b460b43a.js"
+  },
+  {
+    "revision": "dcb4cd64666cd5505096ad597a5b362a",
+    "url": "/react-portfolio/static/media/artem_konstantinov_resume_front_end.dcb4cd64.pdf"
+  }
+]);
